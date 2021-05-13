beee7c308c9ee2ae2bb1550c559249e964bfccd0
Rafael <nem7552@mail.ru>
2021-05-14 01:51:43 +0700

    add homework-2

---
+++ Болтаев_Рафаэль_dz_2/task_2_2.py
@@ -0,0 +1,9 @@
+start_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+for i, s in enumerate(start_list):
+    if s.isdigit():
+        s = f'"{int(s):02d}"'
+    elif s[1:].isdigit():
+        s = f'"{s[0]}{int(s[1:]):02d}"'
+    print(s, end=' ')
+
