a0827f86bcd5575d1983fde927af41bcec76d11d
RafaelBGB <83316636+RafaelBGB@users.noreply.github.com>
2021-05-28 11:12:58 +0700

    Update task_5_3

--- Болтаев_Рафаэль_dz_5/task_5_3.py
+++ Болтаев_Рафаэль_dz_5/task_5_3.py
@@ -1,4 +1,4 @@
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 # result = [12, 44, 4, 10, 78, 123]
 
-result = [print(x, end=' ') for v, x in zip(src, src[1:]) if v < x]
+print([x for v, x in zip(src, src[1:]) if v < x], end=' ')
